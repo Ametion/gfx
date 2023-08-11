706bd214710ceda68287dc40ea0b71ed9b2094cd
Yehor Kochetov <ametionqq@gmail.com>
2023-08-11 22:43:12 +0200

    fix method color in logger

--- gfx/logger.go
+++ gfx/logger.go
@@ -22,12 +22,14 @@ func (l *LoggingResponseWriter) Write(b []byte) (int, error) {
 	n, err := l.ResponseWriter.Write(b)
 
 	if err == nil && l.g.development {
-		color := "\033[33m" // Default to yellow
+		statusColor := "\033[33m" // Default to yellow
 		if l.statusCode == 200 || l.statusCode == 201 {
-			color = "\033[32m" // Green for success
+			statusColor = "\033[32m" // Green for success
 		} else if l.statusCode == 500 || l.statusCode == 400 || l.statusCode == 401 || l.statusCode == 402 || l.statusCode == 403 || l.statusCode == 404 {
-			color = "\033[31m" // Red for errors
+			statusColor = "\033[31m" // Red for errors
 		}
+
+		methodColor := "\033[35m" // Violet for method
 
 		// Bold text
 		bold := "\033[1m"
@@ -35,7 +37,7 @@ func (l *LoggingResponseWriter) Write(b []byte) (int, error) {
 		// Reset formatting after printing
 		reset := "\033[0m"
 
-		fmt.Printf("%sDate: %s, Method: %s, Status code: %s%d%s, Time taken: %v%s\n", bold, time.Now().Format(time.RFC1123), l.method, color, l.statusCode, reset, reset)
+		fmt.Printf("%sDate: %s, Method: %s%s%s, Status code: %s%d%s %v\n", bold, time.Now().Format(time.RFC1123), methodColor, l.method, reset, statusColor, l.statusCode, reset, reset)
 	}
 
 	return n, err
