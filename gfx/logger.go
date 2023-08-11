63f7583f8460fbd319f03febb93a145b9f6d4e84
Yehor Kochetov <ametionqq@gmail.com>
2023-08-11 22:28:34 +0200

    add colors to logger

--- gfx/logger.go
+++ gfx/logger.go
@@ -20,8 +20,23 @@ func (l *LoggingResponseWriter) WriteHeader(statusCode int) {
 
 func (l *LoggingResponseWriter) Write(b []byte) (int, error) {
 	n, err := l.ResponseWriter.Write(b)
+
 	if err == nil && l.g.development {
-		fmt.Printf("Date: %s, Method: %s, Status code: %d\n", time.Now().Format(time.RFC1123), l.method, l.statusCode)
+		color := "\033[33m" // Default to yellow
+		if l.statusCode == 200 || l.statusCode == 201 {
+			color = "\033[32m" // Green for success
+		} else if l.statusCode == 500 || l.statusCode == 400 || l.statusCode == 401 || l.statusCode == 402 || l.statusCode == 403 || l.statusCode == 404 {
+			color = "\033[31m" // Red for errors
+		}
+
+		// Bold text
+		bold := "\033[1m"
+
+		// Reset formatting after printing
+		reset := "\033[0m"
+
+		fmt.Printf("%sDate: %s, Method: %s, Status code: %s%d%s, Time taken: %v%s\n", bold, time.Now().Format(time.RFC1123), l.method, color, l.statusCode, reset, reset)
 	}
+
 	return n, err
 }
